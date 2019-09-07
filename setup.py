cddbf05ac10648339eda4421f022d1944686daab
Bartosz Kościów <kosci1@gmail.com>
2019-09-07 17:28:24 +0200

    multiple encryptors

--- setup.py
+++ setup.py
@@ -13,7 +13,7 @@ def read(*paths):
 
 setup(
     name='iot_message',
-    version='1.0.1',
+    version='1.1.0',
     description='Custom iot:1 protocol',
     keywords=['iot:1'],
     long_description=(read('README.rst')),
